b137696e0c1488eadbffdf8ce9de44fdd6ac0551
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2012-02-03 19:33:12 +0900

    特定回数の生成試行数を超えるとエラーを発生

--- lib/pass.rb
+++ lib/pass.rb
@@ -1,17 +1,21 @@
 
 class Pass
+  NUM_ITERATION = 100
   @list = ('a'..'z').to_a + ('A'..'Z').to_a + ('1'..'9').to_a
   @list.delete_if{|s| ['l','o','I','O','1'].include? s }
 
   def Pass.generate(num = 12)
     raise "Invalid Argument: number of charactors should be more than 1." if num <= 2
+    iteration = 0
     begin
+      raise "Not Converged: #{NUM_ITERATION} times" if iteration > NUM_ITERATION
       pass = ''
       num.times{
         rand_num = rand(@list.size)
         pass += "#{@list[rand_num]}"
       }
-    end until pass =~ /\d/ && pass =~ /[a-z]/
+      iteration += 1
+    end until pass =~ /\d/ && pass =~ /[a-z]/ && pass =~ /[A-Z]/
     pass
   end
 end
--- spec/pass_spec.rb
+++ spec/pass_spec.rb
@@ -24,6 +24,21 @@ describe Pass do
     end
   end
 
+  it "文字列に大文字が1文字以上含まれること" do
+    10.times do
+      (Pass.generate(3) =~ /[A-Z]/).should be_true
+    end
+  end
+
+  it "特定回数の生成試行数を超えるとエラーを発生すること" do
+    Pass::NUM_ITERATION = 1
+    lambda{
+      10.times do
+        Pass.generate(3)
+      end
+    }.should raise_error
+  end
+
   it "2以下の文字数を指定するとエラーを発生すること" do
     lambda{ Pass.generate(2) }.should raise_error
     lambda{ Pass.generate(0) }.should raise_error
